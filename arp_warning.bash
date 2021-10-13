f333e72dda85b6a615b8fe4a85b790de83a4eab2
KiepLangDu <bigfox.ad@gmail.com>
2021-10-13 05:06:11 +0000

    Some bash shell

--- arp_warning.bash
+++ arp_warning.bash
@@ -2,13 +2,15 @@ Update(){
 	devices=$(arp -a|grep -v "incomplete"|grep -v "_gateway"|grep -v "vmnet*"|wc -l)
 }
 Start(){
-	declare -i devices=$(arp -a|grep -v "incomplete"|grep -v "_gateway"|grep -v "vmnet*"|wc -l)
+	date >.MAC
+	devices=$(arp -a|grep -v "incomplete"|grep -v "_gateway"|grep -v "vmnet*"|wc -l)
 	while [ $devices -ge 0 ]
 	do
 		Update
 		while [ $devices -ge 1 ]
 		do
-			zenity --notification --text="Warning! ARP table has $devices strang device(s).\nMaybe you need to take a look for secure."
+			zenity --notification --text="Warning! ARP table has $devices strange device(s).\nMaybe you need to take a look for secure."
+			echo $(arp -a |grep -v "_gateway"|awk {'print $4'};date) >>.MAC
 			ip neigh flush all 
 			ping -c 5 127.0.0.1 >/dev/null
 			Update
